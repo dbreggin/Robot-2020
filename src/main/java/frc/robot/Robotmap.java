5800951dcc52edc53c16fab1982428cdc8bca2f9
etc3po <46801109+etc3po@users.noreply.github.com>
2020-01-18 12:05:07 -0700

    Joystick and Gamepad map

--- src/main/java/frc/robot/Robotmap.java
+++ src/main/java/frc/robot/Robotmap.java
@@ -10,10 +10,44 @@ public final class Robotmap {
     public static final int lMotor1Port = 5;    //2  //TALON SRX
     public static final int lMotor2Port = 0;    //4   //VICTOR SPX
     public static final int lMotor3Port = 6;    //7   //VICTOR SPX
+    
+    //joystick
     public static final int lJoystickPort = 0;
     public static final int joyX = 0;
     public static final int joyY = 1;
     public static final int rJoystickPort = 1;
+    public static final int JoyTrigger = 1;
+    public static final int JoyThumb = 2;
+    public static final int JoyTopL = 5;
+    public static final int JoyTopLB = 3;
+    public static final int JoyTopR = 6;
+    public static final int JoyTopRB = 4;
+    public static final int JoyBotFT = 8;
+    public static final int JoyBotFB = 7;
+    public static final int JoyBotMT = 10;
+    public static final int JoyBotMB = 9;
+    public static final int JoyBotBT = 12;
+    public static final int JoyBotBB = 11;
+
+    //gamepad
     public static final int gamepad = 2;
+    public static final int Abutton = 1;
+    public static final int Bbutton = 2;
+    public static final int Ybutton = 4;
+    public static final int Xbutton = 3;
+    public static final int Rbumper = 6;
+    public static final int Lbumper = 5;
+    public static final int Start = 7;
+    public static final int Select = 8;
+    public static final int L3 = 9;
+    public static final int R3 = 10;
+    public static final int LeftY = 1;
+    public static final int LeftX = 0;
+    public static final int RightX = 4;
+    public static final int RightY = 5;
+    public static final int Rtrigger = 3;
+    public static final int Ltrigger = 2;
+
+
 
 }
