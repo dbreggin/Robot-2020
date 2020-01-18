bc53639976c0e489c5d3565dbfbe7adb94e4ea3a
GhostRunner21 <42250543+GhostRunner21@users.noreply.github.com>
2020-01-18 12:07:41 -0700

    Shuffleboard tings

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -24,6 +24,10 @@ public Joystick gamepad;
 public Joystick rjoystick;
 public Joystick ljoystick;
 public DifferentialDrive drive;
+public double limelightX;
+public double limelightY;
+public double limelightArea;
+public double limelightTarget;
 //Motors 
 private TalonSRX motor1;
 private TalonSRX motor2;
@@ -33,6 +37,7 @@ private TalonSRX motor5;
 private TalonSRX motor6;
 //limelight
 public  NetworkTable  table;
+NetworkTableEntry tv = table.getEntry("tv");
 NetworkTableEntry tx = table.getEntry("tx");
 NetworkTableEntry ty = table.getEntry("ty");
 NetworkTableEntry ta = table.getEntry("ta");
@@ -56,5 +61,16 @@ public OI() {
     lmotor2.follow(lmotor1);
     lmotor3.follow(lmotor1);
     drive = new DifferentialDrive(rmotor1, lmotor1);
+
+    table = NetworkTableInstance.getDefault().getTable("limelight");
+    tx = table.getEntry("tx");
+    ty = table.getEntry("ty");
+    ta = table.getEntry("ta");
+    tv = table.getEntry("tv");
+      
+    limelightX = tx.getDouble(0.0);
+    limelightY = ty.getDouble(0.0);
+    limelightArea = ta.getDouble(0.0);
+    limelightTarget = tv.getDouble(0.0);
     }
 }
