9b33d1acacd6cd861d7a6daf94150c209311ee4b
GhostRunner21 <42250543+GhostRunner21@users.noreply.github.com>
2020-01-17 20:27:21 -0700

    Update OI.java

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -17,12 +17,11 @@ private TalonSRX motor5;
 private TalonSRX motor6;
 //limelight
 public  NetworkTable  table;
-public NetworkTableEntry tx;
-public NetworkTableEntry ty;
-public NetworkTableEntry ta;
-public NetworkTableEntry tv;
-
-
-
-
+NetworkTableEntry tx = table.getEntry("tx");
+NetworkTableEntry ty = table.getEntry("ty");
+NetworkTableEntry ta = table.getEntry("ta");
+//read values periodically
+double xoffset = tx.getDouble(0.0);
+double yoffset = ty.getDouble(0.0);
+double limelightarea = ta.getDouble(0.0);
 }
