11d4bdc1880d5818e8745d2648e973e51d58f267
Arjun624 <arjunavinash0@gmail.com>
2020-02-27 17:43:55 -0700

    Arjun my dad, he told me im poopoo stinky but I took it as a compliment because Arjun is a god

---
+++ src/main/java/frc/robot/commands/FalconEncoder.java
@@ -0,0 +1,11 @@
+package frc.robot.commands;
+import frc.robot.robotmain.Joystickcontrols;
+import frc.robot.robotmain.Buttoncontrols;
+import com.ctre.phoenix.sensors.CANCoder;
+import com.ctre.phoenix.sensors.CANCoderConfiguration;
+import com.ctre.phoenix.sensors.CANCoderFaults;
+import com.ctre.phoenix.sensors.CANCoderStickyFaults;
+
+public final class FalconEncoder {
+        
+}
